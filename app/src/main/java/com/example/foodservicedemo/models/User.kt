ebfabd43c0e38b24370f9821185dde94ac3a39e1
psavvaidis <panagiotissav@gmail.com>
2020-08-04 16:05:51 +0300

    Fix User Model

--- app/src/main/java/com/example/foodservicedemo/models/User.kt
+++ app/src/main/java/com/example/foodservicedemo/models/User.kt
@@ -1,6 +1,7 @@
 package com.example.foodservicedemo.models
 
+
 data class User (
-    private val Name : String = "",
-    val Password : String = ""
+    var Name : String = "",
+    var Password : String = ""
 )
