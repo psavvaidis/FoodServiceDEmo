d7fee55939fdaa770405598730c5222c61ae0bc3
psavvaidis <panagiotissav@gmail.com>
2020-08-04 16:07:05 +0300

    Build Log In Activity

--- app/src/main/java/com/example/foodservicedemo/LogInActivity.kt
+++ app/src/main/java/com/example/foodservicedemo/LogInActivity.kt
@@ -2,11 +2,16 @@ package com.example.foodservicedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.example.foodservicedemo.models.User
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_log_in.*
 
@@ -25,7 +30,19 @@ class LogInActivity : AppCompatActivity() {
             tableUser.addValueEventListener(object: ValueEventListener{
 
                 override fun onDataChange(snapshot: DataSnapshot) {
-                    TODO("Not yet implemented")
+//                    Log.i("User Credentials", "phone : ${edtPhone.text.toString()}, password: ${edtPassword.text.toString()}")
+                    val user: User? = snapshot.child(edtPhone.text.toString()).getValue<User>()
+                        user ?: Toast.makeText(this@LogInActivity, "User Doesn't exist", Toast.LENGTH_LONG).show()
+
+//                    Log.i("User Db Info", "phone : ${user?.Password}, name: ${user?.Name}")
+
+                    if (user!!.Password == edtPassword.text.toString()){
+                        Toast.makeText(this@LogInActivity, "Logged In Successfully", Toast.LENGTH_LONG).show()
+                    }
+                    else{
+                        Toast.makeText(this@LogInActivity, "Couldn't Log In", Toast.LENGTH_LONG).show()
+                    }
+
                 }
 
                 override fun onCancelled(error: DatabaseError) {
